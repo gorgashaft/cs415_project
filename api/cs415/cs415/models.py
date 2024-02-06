ef1d8b1d5de6af4d168c56435675f29ddc025f70
Sergio Li <sel430@ensign.edu>
2024-02-05 17:25:59 -0700

    asdf

--- api/cs415/cs415/models.py
+++ api/cs415/cs415/models.py
@@ -64,7 +64,7 @@ class UserTable(models.Model):
     last_name = models.CharField(max_length=255, blank=True, null=True)
     password = models.CharField(max_length=255, blank=True, null=True)
     username = models.CharField(max_length=255, blank=True, null=True)
-    email = models.CharField(max_length=255, blank=True, null=True)
+    email = models.CharField(max_length=255, blank=True, null=True, unique=True)
     date_created = models.DateField(blank=True, null=True)
 
     class Meta:
