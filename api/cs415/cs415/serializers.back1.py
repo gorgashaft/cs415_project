a3c9d79e5bba8a7284b12854414d387bc5b30f96
Sergio Li <sel430@ensign.edu>
2024-02-09 13:59:45 -0700

    huge change

---
+++ api/cs415/cs415/serializers.back1.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from cs415.models import MovieTable, UserTable, RatingsTable, GenreTable, StudioTable
+
+class UserTableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserTable
+        fields = '__all__'  # Or list the fields you want to include
+
+class MovieTableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovieTable
+        fields = '__all__'  # Or list the fields you want to include
+
+class RatingsTableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RatingsTable
+        fields = '__all__'  # Or list the fields you want to include
+
+class GenreTableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GenreTable
+        fields = '__all__'  # Or list the fields you want to include
+
+class StudioTableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudioTable
+        fields = '__all__'  # Or list the fields you want to include
+
