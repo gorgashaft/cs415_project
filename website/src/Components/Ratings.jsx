b2fbf665da63ffa0e61d1c48b95bd3c11776bd52
Sergio Li <sel430@ensign.edu>
2024-02-09 23:40:00 -0700

    ratings

--- website/src/Components/Ratings.jsx
+++ website/src/Components/Ratings.jsx
@@ -1,34 +1,50 @@
 import React, {useState, useEffect} from 'react'
 import { useNavigate } from "react-router-dom"
 
-const Ratings = () => {
-  const navigate = useNavigate();
-  const [pageName, setPageName] = useState('')
-  const [pageTitle, setPageTitle] = useState('')
-  const [pageDescription, setPageDescription] = useState('')
 
-  useEffect(() => {
-    if (!window.sessionStorage.getItem("auth")) navigate('/unauthorized')
-    fetch('http://localhost:8000/ratings/')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        setPageName(data.page.page_name)
-        setPageTitle(data.page.page_title)
-        setPageDescription(data.page.page_description)
-    })
-    .catch(error => console.error(error));
-}, []);
+const Ratings = () => {
+    const navigate = useNavigate();
+    const [columns, setColumns] = useState([]);
+    const [records, setRecords] = useState([]);
+
+
+    useEffect(() => {
+        if (!window.sessionStorage.getItem("auth")) navigate('/unauthorized')
+        fetch('http://localhost:8000/ratings/')
+        .then(res => res.json())
+        .then(data => {
+            setColumns(Object.keys(data.Ratings[0]))
+            setRecords(data.Ratings)
+        })
+        .catch(error => console.error(error));
+    }, []);
 
   return (
     <div>
-
-      <h1>Movie Ratings</h1>
-      <p><font color="white">This is only accessible when logged in</font></p>
-
-        <h2>{pageName}</h2>
-        <p><b>{pageTitle}</b></p>
-        <p><font color='white'>{pageDescription}</font></p>
+        <h2>Movie Ratings</h2>
+        <table className='table'>
+            <thead>
+                <tr>
+                    {columns.map((c, i) => (
+                        <th key={i}>{c.replaceAll("_", " ").toUpperCase()}</th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    records.map((record,i) => (
+                        <tr key={i}>
+                            <td>{record.rating_id}</td>
+                            <td>{record.value}</td>
+                            <td>{record.movie_id}</td>
+                            <td>{record.user_id}</td>
+                            <td>{record.comments}</td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
+        <button className="link-btn" onClick={() => navigate('/newratingentry')}>+Add New Entry.</button>
     </div>
 
   )
