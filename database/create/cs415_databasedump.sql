2b759f306873f41e59b7d5d65410d7451c6c2b7a
Sergio Li <sel430@ensign.edu>
2024-03-22 13:23:08 -0600

    update

--- database/create/cs415_databasedump.sql
+++ database/create/cs415_databasedump.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `auth_group`\
 --\
 \
+CREATE DATABASE cs415website;
+USE cs415website;
 DROP TABLE IF EXISTS `auth_group`;\
 /*!40101 SET @saved_cs_client     = @@character_set_client */;\
 /*!50503 SET character_set_client = utf8mb4 */;\
